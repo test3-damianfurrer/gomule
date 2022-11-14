0d3a02668ded56ccfd4b136576a480449ba17d05
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-11-14 16:19:18 +0100

    add FileOffer List handling procedure

---
+++ emule/files.go
@@ -0,0 +1,15 @@
+package emule
+
+import (
+	"fmt"
+	"net"
+)
+
+func offerfiles(buf []byte, protocol byte, conn net.Conn, debug bool) {
+	//type=buf[0]
+  count := byteToInt32(buf[1:5])
+  if debug {
+    fmt.Println("DEBUG: files:", count)
+    fmt.Println("DEBUG: metadata:", buf[5:n])
+  }
+}
