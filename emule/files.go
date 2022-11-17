718818a0cb91a17fb5de7285691d1610d9f9d30c
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-11-18 00:01:14 +0100

    pass real buffer length, loopdebug var & add warnings
    debug loop temp disabled
    pass real buffer length & add warnings
    pass newbufferlen
    warnings
    warn on inconsistency with length
    post loop debug

--- emule/files.go
+++ emule/files.go
@@ -5,8 +5,18 @@ import (
 	"net"
 	libdeflate "github.com/4kills/go-libdeflate/v2"
 )
-func prconefile(){
-	
+func prconefile(filehashbuf []byte, filename string, fsize uint32, filetype string, debug bool){
+	fuuid := fmt.Sprintf("%x-%x-%x-%x-%x-%x-%x-%x",
+		filehashbuf[0:2], filehashbuf[2:4], 
+		filehashbuf[4:6], filehashbuf[6:8],
+		filehashbuf[8:10], filehashbuf[10:12], 
+		filehashbuf[12:14], filehashbuf[14:16])
+	if debug {
+    		fmt.Println("DEBUG: File hash:", fuuid)  
+    		fmt.Println("DEBUG: File name:", filename)
+    		fmt.Println("DEBUG: File type:", filetype)
+		fmt.Println("DEBUG: File size:", fsize)
+	}
 }
 
 func prcofferfiles(buf []byte, conn net.Conn, debug bool, blen int) {
@@ -21,54 +31,71 @@ func prcofferfiles(buf []byte, conn net.Conn, debug bool, blen int) {
     fmt.Println("DEBUG: prcofferfiles")
     fmt.Println("DEBUG: files:", count)
   }
-  iteration := 1
+  iteration := 0
   byteoffset := uint32(4)
+  //debugloop:=debug
+  debugloop:=false
   for{
     if byteoffset >= uint32(blen) {
+    //if iteration > 202{
+	    if byteoffset != uint32(blen){
+		    fmt.Println("WARNING: byteoffset doesn't match buffer length", byteoffset, blen)
+	    }
+	    if int32(iteration) != count{
+		    fmt.Println("WARNING: iteration doesn't match filecount", iteration, count)
+	    }
+	    if debug {	
+		    //fmt.Println("DEBUG: exiting, byteoffset >= bufferlength", blen)
+		    fmt.Println("byteoffset", byteoffset)
+		    fmt.Println("iteration", iteration)
+	    }
 	    break;
     }
-    if debug {
+    if debugloop {
       fmt.Println("DEBUG: byteoffset", byteoffset)
       fmt.Println("DEBUG: iteration", iteration)
     }
+//fmt.Println("DEBUG: iteration", iteration)
+//fmt.Println("DEBUG: byte on offset", buf[byteoffset])
     filehashbuf := buf[byteoffset+0:byteoffset+16]
-    fuuid := fmt.Sprintf("%x-%x-%x-%x-%x-%x-%x-%x",
-		filehashbuf[0:2], filehashbuf[2:4], 
-		filehashbuf[4:6], filehashbuf[6:8],
-		filehashbuf[8:10], filehashbuf[10:12], 
-		filehashbuf[12:14], filehashbuf[14:16])
-    fmt.Println("DEBUG: 1.  filehash:", fuuid)
-    fmt.Println("DEBUG: 1. client id:", buf[byteoffset+16:byteoffset+20])
-    fmt.Println("DEBUG: 1. client port:", buf[byteoffset+20:byteoffset+22])
+    
+	 //obfuscated
+    //fmt.Println("DEBUG: client id:", buf[byteoffset+16:byteoffset+20])
+    //fmt.Println("DEBUG: client port:", buf[byteoffset+20:byteoffset+22])
     itag := byteToInt32(buf[byteoffset+22:byteoffset+26])
-    fmt.Println("DEBUG: 1. tag count:", itag)
+    if debugloop {
+    	fmt.Println("DEBUG: 1. tag count:", itag)
+    }
 	  //skip 4 [2 1 0 1] 
     strlen := uint32(byteToInt16(buf[byteoffset+30:byteoffset+32]))
     strbuf := buf[byteoffset+32:byteoffset+32+strlen]
-    str := fmt.Sprintf("%s",strbuf)
-    	  
-    fmt.Println("DEBUG: 1. File name:", str)
+    fname := fmt.Sprintf("%s",strbuf)
     //[3 1 0 2]
     fsize := byteToUint32(buf[byteoffset+32+strlen+4:byteoffset+32+strlen+8])
-    fmt.Println("DEBUG: 1. File size:", fsize)
+   
     if itag > 2 {
     	//[2 1 0 3]
 	strlentype := uint32(byteToInt16(buf[byteoffset+32+strlen+12:byteoffset+32+strlen+14]))
     	strbuf = buf[byteoffset+32+strlen+14:byteoffset+32+strlen+14+strlentype]
-    	str = fmt.Sprintf("%s",strbuf)
-    	fmt.Println("DEBUG: 1. File type:", str)
+    	//str = fmt.Sprintf("%s",strbuf)
+    	
+    	prconefile(filehashbuf, fname, fsize, fmt.Sprintf("%s",strbuf), debugloop)
     	byteoffset = byteoffset+32+strlen+14+strlentype
 	    //in theory needs to be able to handle more tags
     } else {
+    	prconefile(filehashbuf, fname, fsize, "", debugloop)
 	byteoffset = byteoffset+32+strlen+8
     }
     //fmt.Println("DEBUG: 30 bytes more:", buf[byteoffset+36+strlen+14+strlentype:byteoffset+36+strlen+14+strlentype+30])
     iteration+=1
 	  
-    if debug {
+    if debugloop {
       fmt.Println("DEBUG: new byteoffset", byteoffset)
       fmt.Println("DEBUG: next iteration", iteration)
     }
+  }
+  if debug {
+    fmt.Printf("DEBUG: processed %d files and %d bytes\n",iteration,byteoffset)
   }
 }
 func offerfiles(buf []byte, protocol byte, conn net.Conn, debug bool, n int) {
@@ -85,23 +112,32 @@ func offerfiles(buf []byte, protocol byte, conn net.Conn, debug bool, n int) {
 		fmt.Println("ERROR libdeflate:", err.Error())
 		return
   	}
-  	fmt.Println("DEBUG: decompressing")
+	if debug {
+  		fmt.Println("DEBUG: decompressing")
+	}
+	//blen, decompressed, err = dc.Decompress(bufcomp, nil, 1)
   	blen, decompressed, err = dc.Decompress(bufcomp, nil, 1)
 	dc.Close()
-	  fmt.Println("DEBUG: after decompressing")
+	if blen != n-1{
+		fmt.Println("Warning: less bytes processed", blen)
+	}
+	blen=len(decompressed)
+	if debug {
+		fmt.Println("DEBUG: after decompressing")
+	}
 	if err != nil {
 		fmt.Println("ERROR decompress:", err.Error())
 		fmt.Println("ERROR: uncompressed len", blen)
 	  	fmt.Println("ERROR: uncompressed buf 10", decompressed[0:10])
 		return
 	}
-  	fmt.Println("DEBUG: uncompressed len", blen) //9927 vs 9928-1 compressed? lol??? There might be something off
-  	fmt.Println("DEBUG: uncompressed buf 10", decompressed[0:10])
+	fmt.Println("DEBUG: uncompressed bytes", blen)
+  	//fmt.Println("DEBUG: uncompressed buf 10", decompressed[blen+0:blen+10])
 	prcofferfiles(decompressed, conn, debug, blen)
   } else if protocol == 0xe3 {
 	prcofferfiles(bufcomp, conn, debug, n-1)
   } else {
-	  fmt.Println("Error: offerfiles: worong protocol")
+	  fmt.Println("Error: offerfiles: wrong protocol")
   }
 
 }
