c4fc251143dfdafe9f985d9c6aff5f1c28f1b53a
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-11-17 19:57:34 +0100

    removed old code

--- emule/files.go
+++ emule/files.go
@@ -71,6 +71,7 @@ func offerfiles(buf []byte, protocol byte, conn net.Conn, debug bool, n int) {
   	}
   	fmt.Println("DEBUG: decompressing")
   	blen, decompressed, err = dc.Decompress(bufcomp, nil, 1)
+	dc.Close()
 	  fmt.Println("DEBUG: after decompressing")
 	if err != nil {
 		fmt.Println("ERROR decompress:", err.Error())
@@ -86,60 +87,7 @@ func offerfiles(buf []byte, protocol byte, conn net.Conn, debug bool, n int) {
   } else {
 	  fmt.Println("Error: offerfiles: worong protocol")
   }
-	
-	
-if 1 == 2 {
-//initial file offering seems to be always of size 9224 
-  var blen int = 0
-  var decompressed []byte
-  //[]byte decompressed = nil
-	//type=buf[0]
-//it's compressed ...
-  dc, err := libdeflate.NewDecompressor() //not recomended to create a new instance each, but also not possible to use the same simultaniously
-  if err != nil {
-	fmt.Println("ERROR libdeflate:", err.Error())
-	return
-  }
-  fmt.Println("DEBUG: decompressing")
-  //if 1 != 1 {
-  //blen, decompressed, err = dc.DecompressZlib(buf[1:n], nil)
-	//libdeflate.Mode
-  bufcomp := buf[1:n]
-  blen, decompressed, err = dc.Decompress(bufcomp, nil, 1)
-  fmt.Println("DEBUG: after decompressing")
-  if err != nil {
-	fmt.Println("ERROR decompress:", err.Error())
-	fmt.Println("ERROR: uncompressed len", blen)
-  	fmt.Println("ERROR: uncompressed buf 10", decompressed[0:10])
-	return
-  }
-  
-  fmt.Println("DEBUG: uncompressed len", blen)
-  fmt.Println("DEBUG: uncompressed buf 10", decompressed[0:10])
-  //}
-  if 1 != 1 {
-  count := byteToInt32(buf[1:5]) //spec says, can't be more than 200, but is 4 bytes? The resulting number seems utter garbage
-  if debug {
-    fmt.Println("DEBUG: type:", buf[0])
-    fmt.Println("DEBUG: files:", count)
-    //fmt.Println("DEBUG: metadata:", buf[5:n])
-    fuuid := fmt.Sprintf("%x-%x-%x-%x-%x-%x-%x-%x",
-		buf[5:7], buf[7:9], buf[9:11], buf[11:13], buf[13:15], buf[15:17], buf[17:19],
-		buf[19:21])
-    fmt.Println("DEBUG: 1.  filehash:", buf[5:21])
-    fmt.Println("DEBUG: 1.  filehash:", fuuid)
-    fmt.Println("DEBUG: 1. client id:", buf[21:25])
-    cport := byteToInt16(buf[25:27])
-    fmt.Println("DEBUG: 1. client port:", buf[25:27])
-    fmt.Println("DEBUG: 1. client port:", cport)
-    itag := byteToInt32(buf[27:31])
-    fmt.Println("DEBUG: 1. tag count:", buf[27:31])
-    fmt.Println("DEBUG: 1. tag count:", itag)
-    fmt.Println("DEBUG: 10 bytes more:", buf[31:41])
-  }
-  }
-  dc.Close()
-}
+
 }
 
 func filesources(buf []byte, protocol byte, conn net.Conn, debug bool, n int) {
