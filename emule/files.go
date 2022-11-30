8797bc7d42a7e58826565e59f34f7bdb0876939a
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-11-30 09:29:26 +0100

    responsed data debug - max 255 sources

--- emule/files.go
+++ emule/files.go
@@ -203,7 +203,7 @@ func filesources(buf []byte, protocol byte, conn net.Conn, debug bool, n int, db
 	  
     //fmt.Println("DEBUG: full buf:", n, buf[0:n])	  
   }
-  data = make([]byte, 0)
+data := make([]byte, 0)
   listitems, srcdata:=queryfilesources(buf[1:17],debug,db) //valid hash
   if debug {
     fmt.Println("DEBUG: found sources: ",listitems)
@@ -213,9 +213,14 @@ func filesources(buf []byte, protocol byte, conn net.Conn, debug bool, n int, db
   //protocol 0xE3, found sources type 0x42
   msgsize := uint32(listitems)*uint32(6)
   msgsize += uint32(17)
-  data = append(data,protocol,uint32ToByte(msgsize)...,0x42,buf[1:17]...,srcdata...)
+  data = append(data,protocol)
+  data = append(data,uint32ToByte(msgsize)...)
+  data = append(data,0x42)
+  data = append(data,buf[1:17]...) //file hash
+  data = append(data,byte(listitems))   // count of sources, just one byte? - limit 255 in sql querry
+  data = append(data,srcdata...)
   if debug {
-    fmt.Println("DEBUG: sources answer: ",data)
+    fmt.Println("DEBUG: sources answer: ",data) //fmt.Println("DEBUG: sources answer: ",data[1:30])
   }
 }
 
@@ -225,7 +230,7 @@ func queryfilesources(filehash []byte, debug bool, db *sql.DB) (listitems int, s
     srcuhash := make([]byte, 16)
     var ed2kid uint32
     var port int16 //var port uint16
-    rows, err := db.Query("select sources.user_hash,clients.id_ed2k,clients.port from sources left join clients on sources.user_hash=clients.hash where sources.file_hash = ?", filehash)
+    rows, err := db.Query("select sources.user_hash,clients.id_ed2k,clients.port from sources left join clients on sources.user_hash=clients.hash where sources.file_hash = ? LIMIT 255", filehash)
 	//INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
     if err != nil {
 	fmt.Println("ERROR: ",err.Error())
