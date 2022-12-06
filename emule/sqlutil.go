f4a627fa6fb6002479fc6680093b926a75f1f887
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 08:45:16 +0100

    add file_hash to query

--- emule/sqlutil.go
+++ emule/sqlutil.go
@@ -7,8 +7,27 @@ import (
   	"strings"
 )
 
+//Field   Type    Null    Key     Default Extra
+//id      bigint unsigned NO      PRI     NULL    auto_increment
+//file_hash       binary(16)      NO      MUL     0x30
+//user_hash       binary(16)      NO              0x30
+//name    varchar(255)    NO      MUL
+//ext     varchar(8)      NO
+//time_offer      timestamp       NO              CURRENT_TIMESTAMP       DEFAULT_GENERATED
+//type    enum('Image','Audio','Video','Pro','Doc','')    NO
+//rating  tinyint unsigned        NO              0
+//title   varchar(128)    NO
+//artist  varchar(128)    NO
+//album   varchar(128)    NO
+//length  int unsigned    NO              0
+//bitrate int unsigned    NO              0
+//codec   varchar(32)     NO
+//online  tinyint(1)      NO              0
+//complete        tinyint(1)      NO              0
+
+
 func search2query(search string)(sqlquery string, strarr []string){
-  sqlquery = "select name, ext, type, rating from sources WHERE "
+  sqlquery = "select name, ext, type, rating, file_hash from sources WHERE "
   strarr = strings.Split(search," ")
   for i := 0; i < len(strarr); i++ {
 	  strarr[i] = "%"+strarr[i]+"%"
