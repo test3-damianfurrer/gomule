5674475209a5ea8daa90a5e6062236b8d3a9ce09
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 06:31:06 +0100

    Update sqlutil.go

--- emule/sqlutil.go
+++ emule/sqlutil.go
@@ -7,12 +7,16 @@ import (
   	"strings"
 )
 
-func search2query(search string)(sqlquery string){
+func search2query(search string)(sqlquery string, strarr []string){
   sqlquery = "select name, ext, type, rating from sources WHERE "
-  strarr := strings.Split(search," ")
+  strarr = strings.Split(search," ")
   for i := 0; i < len(strarr); i++ {
 	  strarr[i] = "%"+strarr[i]+"%"
-	  sqlquery += "name like ? AND "
+	  if i < len(strarr)-1 {
+	  	sqlquery += "name like ? AND "
+	  } else {
+		sqlquery += "name like ?"
+	  }
 	  fmt.Println("String: ",i,strarr[i])
   }
   fmt.Println("query: ",strarr)
