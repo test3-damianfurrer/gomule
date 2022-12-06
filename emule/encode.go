2767f8f129c4d61e41d6e883e336472f421c05e2
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 13:40:37 +0100

    add func encodeByteString

--- emule/encode.go
+++ emule/encode.go
@@ -5,16 +5,27 @@ import (
 
 func encodeByteMsg(protocol byte,msgcode byte,body []byte) []byte {
 	bodysize := len(body)
-	size := uint32ToByte(uint32(bodysize+1))
+	sizebytes := uint32ToByte(uint32(bodysize+1))
 	buf := make([]byte,bodysize+6)
 	buf[0] = protocol
-	buf[1] = size[0]
-	buf[2] = size[1]
-	buf[3] = size[2]
-	buf[4] = size[3]
+	buf[1] = sizebytes[0]
+	buf[2] = sizebytes[1]
+	buf[3] = sizebytes[2]
+	buf[4] = sizebytes[3]
 	buf[5] = msgcode
 	for i := 0; i < bodysize; i++ {
 		buf[i+6] = body[i]
 	}
 	return buf
+}
+
+func encodeByteString(string str) []byte {
+	slen:=len(str)
+	buf := make([]byte,slen+2)
+	sizebytes := uint16ToByte(uint16(slen))
+	buf[0] = sizebytes[0]
+	buf[1] = sizebytes[1]
+	for i := 0; i < slen; i++ {
+		buf[i+2] = str[i]
+	}
 }
