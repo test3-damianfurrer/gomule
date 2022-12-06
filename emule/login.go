a41383362bde01f68df4b0978b45014b0aef5fee
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 16:20:18 +0100

    append tags

--- emule/login.go
+++ emule/login.go
@@ -130,13 +130,17 @@ func login(buf []byte, protocol byte, conn net.Conn, debug bool, db *sql.DB, shi
 	serverip_b:=uint32ToByte(shighid)
 	serverport_b:=uint16ToByte(sport)
 	serverguid_b := make([]byte,16)
-	tagcount_b := uint32ToByte(uint32(0))
+	tagcount_b := uint32ToByte(uint32(2))
 	iddata := make([]byte,0)
 	
 	iddata=append(iddata,serverguid_b...)
 	iddata=append(iddata,serverip_b...)
 	iddata=append(iddata,serverport_b...)
 	iddata=append(iddata,tagcount_b...)
+	servname := encodeByteTagString(encodeByteTagNameInt(0x1),"Servername")
+	servdesc := encodeByteTagString(encodeByteTagNameInt(0xb),"Serverdesc")
+	iddata=append(iddata,servname...)
+	iddata=append(iddata,servdesc...)
 	if debug {
 		fmt.Println("DEBUG: serverguid_b:", serverguid_b)
 		fmt.Println("DEBUG: serverip_b:", serverip_b)
