4699cda1ade8ba4dd1e216dbb10d157e7f39c238
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 06:10:55 +0100

    Update sqlutil.go

--- emule/sqlutil.go
+++ emule/sqlutil.go
@@ -10,6 +10,9 @@ import (
 func search2query(search string)(sqlquery string){
   //sqlquery = "test"
   strarr := strings.Split(search," ")
+  for i := 0; i < len(strarr); i++ {
+	  fmt.Println("String: ",i,strarr[i])
+  }
   fmt.Println("query: ",strarr)
   return
 }
