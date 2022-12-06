b82ec6a7f9c3cfc8bb522bdbda02044fba6d9c51
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 19:15:24 +0100

    Update decode.go

--- emule/decode.go
+++ emule/decode.go
@@ -21,6 +21,7 @@ func readString(pos int, buf []byte)(bread int, ret string) {
 }
 
 func readTag(pos int, buf []byte)(bread int, ret *OneTag) {
+  fmt.Println("readtag!",buf[pos:len(buf)])
   ret = &OneTag{Type: buf[pos], NameString: ""}
   bread=3
   readname:=0
