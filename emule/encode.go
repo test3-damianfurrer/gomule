ded00a81c9f987e3a420da8306bd90503e003e48
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 13:07:15 +0100

    add body with loop

--- emule/encode.go
+++ emule/encode.go
@@ -5,9 +5,11 @@ import (
 )
 
 func encodeByteMsg(protocol byte,msgcode byte,body []byte) []byte {
-  size := uint32ToByte(uint32(len(body)))
-  return []byte{protocol,
-    size[0], size[1], size[2], size[3],
-    msgcode,
-		body...}
+	size := uint32ToByte(uint32(len(body)))
+	buf := [size+6]byte{protocol, size[0], size[1], size[2], size[3],msgcode}
+		//body...
+	for i := 0; i < len(size); i++ {
+		data[i+6] = body[i]
+	}
+	return buf
 }
