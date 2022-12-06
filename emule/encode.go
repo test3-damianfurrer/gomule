972380a5837fd503f06a81428c327bddc2e91993
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 13:24:29 +0100

    use correct var

--- emule/encode.go
+++ emule/encode.go
@@ -1,13 +1,12 @@
 package emule
 
 import (
-	"fmt"
 )
 
 func encodeByteMsg(protocol byte,msgcode byte,body []byte) []byte {
 	bodysize := len(body)
 	size := uint32ToByte(uint32(bodysize+1))
-	buf := [size+6]byte{protocol, size[0], size[1], size[2], size[3],msgcode}
+	buf := [bodysize+6]byte{protocol, size[0], size[1], size[2], size[3],msgcode}
 		//body...
 	for i := 0; i < len(size); i++ {
 		buf[i+6] = body[i]
