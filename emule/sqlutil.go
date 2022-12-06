29d7682b12fecb34330413d5f07a8ed9168f10df
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 06:23:06 +0100

    gen query

--- emule/sqlutil.go
+++ emule/sqlutil.go
@@ -8,9 +8,11 @@ import (
 )
 
 func search2query(search string)(sqlquery string){
-  //sqlquery = "test"
+  sqlquery = "select name, ext, type, rating from sources WHERE "
   strarr := strings.Split(search," ")
   for i := 0; i < len(strarr); i++ {
+	  strarr[i] = "%"+strarr[i]+"%"
+	  sqlquery += "name like ? AND "
 	  fmt.Println("String: ",i,strarr[i])
   }
   fmt.Println("query: ",strarr)
