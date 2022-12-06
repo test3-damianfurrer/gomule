41b99a4c8c96df2af2f26884df20f1dd1087f5f2
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-06 13:30:47 +0100

    Update encode.go

--- emule/encode.go
+++ emule/encode.go
@@ -6,8 +6,13 @@ import (
 func encodeByteMsg(protocol byte,msgcode byte,body []byte) []byte {
 	bodysize := len(body)
 	size := uint32ToByte(uint32(bodysize+1))
-	buf := [bodysize+6]byte{protocol, size[0], size[1], size[2], size[3],msgcode}
-		//body...
+	buf := make([]byte,bodysize+6)
+	buf[0] = protocol
+	buf[1] = size[0]
+	buf[2] = size[1]
+	buf[3] = size[2]
+	buf[4] = size[3]
+	buf[5] = msgcode
 	for i := 0; i < len(size); i++ {
 		buf[i+6] = body[i]
 	}
