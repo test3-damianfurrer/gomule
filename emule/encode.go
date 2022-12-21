504a6801739ce1d6eb5c9424d04bcaa6e59527be
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2022-12-21 16:12:10 +0100

    Search and other Util Functions created/extended
    Mysql debug file search - search string & fixed debugging printing (#155)
    Mysql debug file search - search string
    
    test func dbsearchfiles - query and print according to search string(simplistic space = AND, tags and ext. search syntax ignored so far)
    added file sqlutil for query construction
    get and send real file and user count
    use function to encode a hardcoded server message string into a valid byte array
    Add Decode & Encode Functions, Custom Server Strings & use with login (#161)
    adjusted server message
    server string parameters
    set server strings in gomule.go
    readTags & use in Login function
    
    decode funcs:
    enumNumberConstraint
    enumStringConstraint
    readConstraints
    
    File func:
    dbsearchfilesexec
    
    sql util funcs:
    stringifyConstraint
    constraintsearch2query
    upd func names, tcpflags & offerfiles readTags, add file ext to db (#166)
    added filename2ext function
    upd func names, tcpflags & offerfiles readTags
    enable large file support
    stringifyConstraint add min and max size
    extend constraints
    use experimental query
    add checks, new search2query2, dbsearchfilesexec sends results & added TODO 
    Fixsources, prevent out of buffer bounds and check more error
    use SliceBuf to prevent read over buf len
    capture net.ErrClosed (wanted) disaple printing of err in the read func
    Fixsearch , new search2query2, dbsearchfilesexec sends results & added TODO 
    new func search2query2 with params interface
    use search2query2 and dbsearchfilesexec directly
    added Response for Search request
    pass conn version
    fix wrong type number sent
    Create TODO.md

---
+++ emule/encode.go
@@ -0,0 +1,107 @@
+package emule
+
+import (
+)
+
+func EncodeByteMsg(protocol byte,msgcode byte,body []byte) []byte {
+	bodysize := len(body)
+	sizebytes := UInt32ToByte(uint32(bodysize+1))
+	buf := make([]byte,bodysize+6)
+	buf[0] = protocol
+	buf[1] = sizebytes[0]
+	buf[2] = sizebytes[1]
+	buf[3] = sizebytes[2]
+	buf[4] = sizebytes[3]
+	buf[5] = msgcode
+	for i := 0; i < bodysize; i++ {
+		buf[i+6] = body[i]
+	}
+	return buf
+}
+
+func EncodeByteString(str string) []byte {
+	slen:=len(str)
+	buf := make([]byte,slen+2)
+	sizebytes := UInt16ToByte(uint16(slen))
+	buf[0] = sizebytes[0]
+	buf[1] = sizebytes[1]
+	for i := 0; i < slen; i++ {
+		buf[i+2] = str[i]
+	}
+	return buf
+}
+
+
+//func EncodeByteTag(ttype byte, tagname []byte, tagvalue []byte, specialdesignator byte){
+//	
+//}
+
+func EncodeByteTagString(tagname []byte, tagvalue string) []byte {
+	return EncodeByteTag(2,tagname,EncodeByteString(tagvalue))
+}
+
+func EncodeByteTagInt(tagname []byte, tagvalue uint32) []byte {
+	return EncodeByteTag(3,tagname,UInt32ToByte(tagvalue))
+}
+
+/*func EncodeByteTagInt(tagname []byte, tagvalue float) []byte {
+	return EncodeByteTag(4,tagname,uInt32ToByte(tagvalue))
+}*/
+
+func EncodeByteTag(ttype byte, tagname []byte, tagvalue []byte) []byte {
+	//buflen=len(tagname)+len(tagvalue)+1
+	buf := make([]byte,0)
+	buf = append(buf, ttype)
+	buf = append(buf, tagname...)
+	buf = append(buf, tagvalue...)
+	return buf
+	
+	/*
+	Login example: 
+	vers tag:   [3 1 0 17 60 0 0 0]
+	port tag:   [3 1 0 32 29 3 0 0] -> 0x20 (port should be 0x0f, 15)
+	flag tag:   [3 1 0 251 128 13 4 3] //seems to be some other tag (should be 0x20)
+	name tag: [2 1 0 1 (strlen)(string) ]
+	2/3 = type string/int
+	[1 0] = bytes for tag name
+	17(0x11) = tag name value for Version Tag
+
+	Offer files example(somehow reversed?):
+	[1 4 0 116 101 115 116] //simple
+	[0 0 1 4 0 116 101 115 116 2 5 0 73 109 97 103 101 1 0 3]
+	initial 0 0 - not sure, marking complex search and maybe something else
+	type, value(len, str), tagname
+	string type, 1 0 3 = Filtype tag name
+	value = "Image"(5)
+	
+	*/
+}
+
+func EncodeByteTagNameInt(val byte) []byte {
+	buf := make([]byte,1)
+	buf[0]=val
+	return EncodeByteTagName(buf)
+}
+func StringToByte(val string) []byte {
+	strlen:=len(val)
+	buf := make([]byte,strlen)
+	for i := 0; i < strlen; i++ {
+		buf[i] = val[i]
+	}
+	return buf
+}
+func EncodeByteTagNameStr(val string) []byte {
+	return EncodeByteTagName(StringToByte(val))
+}
+
+func EncodeByteTagName(nbuf []byte) []byte {
+	blen:=len(nbuf)
+	buf := make([]byte,blen+2)
+	sizebytes := UInt16ToByte(uint16(blen))
+	buf[0] = sizebytes[0]
+	buf[1] = sizebytes[1]
+	for i := 0; i < blen; i++ {
+		buf[i+2] = nbuf[i]
+	}
+	return buf
+}
---
+++ emule/sqlutil.go
@@ -0,0 +1,142 @@
+package emule
+
+import (
+	"fmt"
+	//"net"
+	"database/sql"
+  	"strings"
+)
+
+//Field   Type    Null    Key     Default Extra
+//id      bigint unsigned NO      PRI     NULL    auto_increment
+//file_hash       binary(16)      NO      MUL     0x30
+//user_hash       binary(16)      NO              0x30
+//name    varchar(255)    NO      MUL
+//ext     varchar(8)      NO
+//time_offer      timestamp       NO              CURRENT_TIMESTAMP       DEFAULT_GENERATED
+//type    enum('Image','Audio','Video','Pro','Doc','')    NO
+//rating  tinyint unsigned        NO              0
+//title   varchar(128)    NO
+//artist  varchar(128)    NO
+//album   varchar(128)    NO
+//length  int unsigned    NO              0
+//bitrate int unsigned    NO              0
+//codec   varchar(32)     NO
+//online  tinyint(1)      NO              0
+//complete        tinyint(1)      NO              0
+func filename2ext(filename string) string {
+	strarr := strings.Split(filename,".")
+	sindex := len(strarr)-1
+	if sindex == 0 {
+		return ""
+	}
+	slen := len(strarr[sindex])
+	if slen > 8 {
+		return ""
+	}
+	return strings.ToUpper(strarr[sindex])
+}
+
+func stringifyConstraint(in *Constraint, params *[]interface{})(ret string){
+	switch in.Type {
+		/*
+		C_FILETYPE
+		C_FILEEXT
+		C_AVAIL
+		*/
+		case C_AND:
+			ret = "("+stringifyConstraint(in.Left,params)+") AND ("+stringifyConstraint(in.Right,params)+")"
+		case C_OR:
+			ret = "("+stringifyConstraint(in.Left,params)+") OR ("+stringifyConstraint(in.Right,params)+")"
+		case C_NOT:
+			ret = "("+stringifyConstraint(in.Left,params)+") NOT ("+stringifyConstraint(in.Right,params)+")"
+		case C_MAIN:
+			strarr := strings.Split(fmt.Sprintf("%s",in.Value)," ")
+			ret = ""
+  			for i := 0; i < len(strarr); i++ {
+				if i != 0 {
+					ret += " AND "
+				}
+				ret += "sources.name like ?"
+				*params = append(*params,"%"+strarr[i]+"%")
+			}
+		case C_AVAIL:
+		case C_CODEC:
+			*params = append(*params,fmt.Sprintf("%s",in.Value))
+			ret = "sources.codec like ?"
+		case C_MINSIZE:
+			*params = append(*params,ByteToUint32(in.Value))
+			fmt.Println("DEBUG(sqlutil.go): minsize ",ByteToUint32(in.Value),in.Value)
+			ret = "files.size >= ?"
+		case C_MAXSIZE:
+			*params = append(*params,ByteToUint32(in.Value))
+			fmt.Println("DEBUG(sqlutil.go): maxsize ",ByteToUint32(in.Value),in.Value)
+			ret = "files.size <= ?"
+		case C_FILETYPE:
+			*params = append(*params,fmt.Sprintf("%s",in.Value))
+			ret = "sources.type = ?"
+		case C_FILEEXT:
+			*params = append(*params,fmt.Sprintf("%s",in.Value))
+			ret = "sources.ext like ?"
+		default:
+			fmt.Println("ERROR: undefined Constraint Type", in.Type)
+	}
+	return
+}
+func constraintsearch2query(in *Constraint, params *[]interface{})(sqlquery string){
+	fields := "sources.name, sources.ext, sources.type, sources.rating, sources.file_hash, files.size"
+	constraints := stringifyConstraint(in, params)
+	sqlquery = "select " + fields + " from sources left join files on sources.file_hash=files.hash WHERE "
+	sqlquery += constraints
+	sqlquery2 := "select count(sources.id), " + fields + " from sources left join files on sources.file_hash=files.hash WHERE "
+	sqlquery2 += constraints
+	sqlquery2 = sqlquery2 + " group by " + fields + " "
+	fmt.Println("DEBUG: QUERY2: ",sqlquery2) //availability would have to be having after where
+	sqlquery = sqlquery2
+	return
+}
+
+func search2query2(search string,params *[]interface{})(sqlquery string){
+  fields := "sources.name, sources.ext, sources.type, sources.rating, sources.file_hash, files.size"
+  sqlquery = "select count(sources.id), "+fields+" from sources left join files on sources.file_hash=files.hash WHERE "
+  strarr := strings.Split(search," ")
+  for i := 0; i < len(strarr); i++ {
+	  *params = append(*params,"%"+strarr[i]+"%")
+	  if i < len(strarr)-1 {
+	  	sqlquery += "sources.name like ? AND "
+	  } else {
+		sqlquery += "sources.name like ?"
+	  }
+	  fmt.Println("String: ",i,strarr[i])
+  }
+  sqlquery += " group by " + fields
+  fmt.Println("DEBUG: simple query: ",sqlquery)
+  return
+}
+
+func search2query(search string)(sqlquery string, strarr []string){
+  fields := "sources.name, sources.ext, sources.type, sources.rating, sources.file_hash, files.size"
+  sqlquery = "select count(sources.id), "+fields+" from sources left join files on sources.file_hash=files.hash WHERE "
+  strarr = strings.Split(search," ")
+  for i := 0; i < len(strarr); i++ {
+	  strarr[i] = "%"+strarr[i]+"%"
+	  if i < len(strarr)-1 {
+	  	sqlquery += "sources.name like ? AND "
+	  } else {
+		sqlquery += "sources.name like ?"
+	  }
+	  fmt.Println("String: ",i,strarr[i])
+  }
+  sqlquery += " group by " + fields
+  fmt.Println("query: ",strarr)
+  return
+}
+
+func readRowUint32(query string,db *sql.DB) uint32 {
+	var value uint32
+	err := db.QueryRow(query).Scan(&value)
+    	if err != nil {
+		fmt.Println("ERROR(readRowUint32): ",err.Error())
+	}
+	return value
+}
